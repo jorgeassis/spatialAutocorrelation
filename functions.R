0a76cc14c6f5e9bc087b8fb41c79a80d7e3f24b2
Jorge Assis <jorgeassis@Jorges-MacBook.Home>
2020-08-06 00:00:00 +0100

    Up.

--- functions.R
+++ functions.R
@@ -1,3 +1,141 @@
 
-source("https://raw.githubusercontent.com/jorgeassis/marineforestsDB/master/sourceMe.R")
+spatialAutocorrelation <- function(occurrenceRecords,rasterLayers,autocorrelationClassDistance,autocorrelationMaxDistance,autocorrelationSignif) {
+  
+  set.seed(42)
+  
+  if(nrow(occurrenceRecords) > 1000) { 
+    
+    cat( paste0("\n"))
+    cat( paste0("\n"))
+    cat("More than 1000 occurrence records.","\n")  
+    cat("Using a maximum of 1000 random records.","\n")
+    
+    occurrenceRecords <- occurrenceRecords[sample(1:nrow(occurrenceRecords),1000,replace = FALSE),]
+    
+  }
+  
+  occurrenceRecords <- data.frame(Lon=as.numeric(as.character(occurrenceRecords[,1])),Lat=as.numeric(as.character(occurrenceRecords[,2])))
+  to.keep <- which(!duplicated(occurrenceRecords[,1]))
+  occurrenceRecords <- occurrenceRecords[to.keep,]
+    
+  # --------
+  
+  presences.environment <- data.frame(raster::extract(rasterLayers,occurrenceRecords,stringsAsFactors=FALSE))
+  to.keep <- which(!is.na(presences.environment[,1]))
+  presences.environment <- presences.environment[to.keep,]
+  occurrenceRecords <- occurrenceRecords[to.keep,]
+  
+  # --------
+
+  space <- spDists(as.matrix(occurrenceRecords),as.matrix(occurrenceRecords),longlat=TRUE)
+  data <- ecodist::distance( presences.environment , method = "euclidean")
+  data <- as.matrix(data)
+  
+  n.class <- round(autocorrelationMaxDistance / autocorrelationClassDistance)
+  
+  resultsMatrix <- data.frame(classdistanceFrom=seq(0,autocorrelationMaxDistance-autocorrelationClassDistance,autocorrelationClassDistance),
+                              classdistanceTo=seq(autocorrelationClassDistance,autocorrelationMaxDistance,autocorrelationClassDistance),
+                              R=NA,
+                              pVal=NA)
+  
+  for( i in 1:nrow(resultsMatrix)) {
+    
+    d1 = resultsMatrix[i,1]
+    d2 = resultsMatrix[i,2]
+    
+    data.d <- as.vector(data)
+    space.d <- as.vector(space)
+    
+    remove <- which(space.d < d1 | space.d > d2)
+    data.d <- data.d[-remove]
+    space.d <- space.d[-remove]
+
+    modelobject <- lm(space.d~data.d)
+    
+    f <- summary(modelobject)$fstatistic
+    p <-0
+    
+    tryCatch( p <- pf(f[1],f[2],f[3],lower.tail=FALSE) , error=function(e) { Error <<- TRUE })
+    
+    #p <- summary.lm(modelobject)$coefficients[2,"Pr(>|t|)"] 
+    resultsMatrix[i,3] <- summary(modelobject)$adj.r.squared
+    resultsMatrix[i,4] <- p
+    
+  }
+  
+  vect.signif <- as.numeric(resultsMatrix[,4] > autocorrelationSignif)
+  if(vect.signif[1] == 1) { vect.signif[1] <- 0}
+  vect.signif.pch <- c(19,1)
+  
+  par(mar = c(4.5, 5.5, 4.5, 4.5) , bg = "#FFFFFF")
+  plot(resultsMatrix[,2],resultsMatrix[,3],axes=FALSE,xlab="Distance (Km)",ylab="")
+  rect(par("usr")[1], par("usr")[3], par("usr")[2], par("usr")[4], col = "#F6F6F6")
+  lines(resultsMatrix[,2],resultsMatrix[,3],lty=2,col="#000000",type="l",xlab="Distance (Km)",ylab="")
+  
+  points(resultsMatrix[,2],resultsMatrix[,3],pch=vect.signif.pch[vect.signif+1],col="#5B5B5B")
+  axis(2,las=2,col="White",col.ticks="Black")
+  axis(1,las=0,col="White",col.ticks="Black")
+  box()
+  title(ylab="Correlation (R)",mgp=c(4,1,0)) 
+  abline(h = 0,lty=1)
+  
+  distance <- round( resultsMatrix[ which(resultsMatrix[,4] >= autocorrelationSignif)  , 2 ][1] )
+  if( is.na(distance)) { distance <- autocorrelationMaxDistance }
+  cat( paste0("\n"))
+  cat( paste0("\n"))
+  cat( paste0("First non-correlated distance: ",distance," km"))
+  
+  return(distance)
+  
+}
+
+
+spatialThinning <- function(occurrenceRecords,rasterLayers,minDistance,verbose) {
+  
+  if( missing(verbose) ) { verbose <- TRUE }
+  
+  occurrenceRecords <- data.frame(Lon=as.numeric(as.character(occurrenceRecords[,1])),Lat=as.numeric(as.character(occurrenceRecords[,2])))
+  to.keep <- which(!duplicated(occurrenceRecords[,1]))
+  shape <- subset(rasterLayers,1)
+  
+  occurrenceRecords.i <- rasterize(occurrenceRecords,shape)
+  occurrenceRecords.i <- xyFromCell(occurrenceRecords.i, Which(!is.na(occurrenceRecords.i),cells=TRUE) )
+  colnames( occurrenceRecords.i ) <- c( "Lon", "Lat" )
+  
+  dataThinning <- data.frame(Name="Sp",occurrenceRecords.i)
+  
+  coordinates.t <- thin(dataThinning,
+                        lat.col = "Lat",
+                        long.col = "Lon",
+                        spec.col = "Name",
+                        thin.par = minDistance,
+                        reps = 1,
+                        write.files = FALSE,
+                        locs.thinned.list.return = TRUE,
+                        verbose = FALSE)[[1]]
+  
+  if(verbose) {
+    
+    cat( paste0("\n"))
+    cat( paste0("\n"))    
+    cat( paste0("Input Records: ",nrow(occurrenceRecords)))
+    cat( paste0("\n"))
+    cat( paste0("Final Records: ",nrow(coordinates.t)))
+    
+  }
+  
+  # Remove from main dataset of occurrences
+  colnames( coordinates.t ) <- c( "Lon", "Lat" )
+  
+  # Remove log file
+  if(length(list.files(".", full.names = TRUE, pattern = "spatial_thin_log.txt"))){
+    file.remove( list.files(".", full.names = TRUE, pattern = "spatial_thin_log.txt") ) 
+  }
+  
+  return(coordinates.t)
+  
+}
+
+
+
 
--- mainScript.R
+++ mainScript.R
@@ -1,29 +1,42 @@
 # -----------------------------------------------
-# Read libraries
+# Read libraries and main functions
 
 library(raster)
+library(ggplot2)
+library(sdmpredictors)
+library(ecodist)
+library(sp)
+library(spThin)
 
-# -----------------------------------------------
-# Read functions
-
-source("https://raw.githubusercontent.com/jorgeassis/spatialAutocorrelaiton/master/functions.R")
+source("functions.R")
 
 # -----------------------------------------------
 # Read occurrence records from Assis et al. (2020)
 # Assis, J., Fragkopoulou, E., Frade, D. et al. A fine-tuned global distribution dataset of marine forests. Sci Data 7, 119 (2020). https://doi.org/10.1038/s41597-020-0459-x
 
+source("https://raw.githubusercontent.com/jorgeassis/marineforestsDB/master/sourceMe.R")
+
 dataset <- extractDataset("brownAlgae",pruned=TRUE)
-occurrenceRecords <- subsetDataset(dataset,taxa="Laminaria digitata",status="accepted")
+occurrenceRecords <- subsetDataset(dataset,taxa="Laminaria ochroleuca",status="accepted")
+
+# Make sure to use a Lon-Lat data.frame
+occurrenceRecords <- data.frame(Lon=as.numeric(as.character(occurrenceRecords[,"decimalLongitude"])),Lat=as.numeric(as.character(occurrenceRecords[,"decimalLatitude"])))
 
 # -----------------------------------------------
 # Read raster from Assis et al. (2018)
 # Assis, J, Tyberghein, L, Bosch, S, Verbruggen, H, Serrão, EA, De Clerck, O. Bio‐ORACLE v2.0: Extending marine data layers for bioclimatic modelling. Global Ecol Biogeogr. 2018; 27: 277– 284. https://doi.org/10.1111/geb.12693
 
+rasterLayers <- load_layers(c("BO2_tempmax_ss","BO2_chlomax_bdmean","BO2_salinitymean_ss","BO2_phosphatemean_ss"),equalarea=FALSE) 
+names(rasterLayers) <- c("Temperature","Chlorophyll","Salinity","Phosphate")
 
+# Define the distance class 
+autocorrelationClassDistance <- 5
 
+# Define the maximum distance at which autocorrelation will be determined
+autocorrelationMaxDistance <- 500
 
-
-
+# Define the significance level of the test
+autocorrelationSignif <- 0.05
 
 distanceUncorr <- data.frame(Predictor=names(rasterLayers),Distance=NA)
 for( i in 1:length(names(rasterLayers))) {
@@ -33,7 +46,7 @@ for( i in 1:length(names(rasterLayers))) {
 distanceUncorrPlot <- ggplot(distanceUncorr[sort(distanceUncorr[,2],decreasing = TRUE,index.return=TRUE)$ix,]) +
   geom_bar( aes(x= reorder(Predictor, Distance) , y=Distance), stat="identity", fill="black", alpha=0.5) +
   coord_flip() + theme(
-    axis.text=element_text(size=10),
+    axis.text=element_text(size=12),
     axis.title.x = element_text(margin = margin(t = 10, r = 0, b = 0, l = 0) , size=12),
     axis.title.y = element_text(margin = margin(t = 0, r = 10, b = 0, l = 0) , size=12),
     panel.border = element_blank(),
@@ -44,16 +57,12 @@ distanceUncorrPlot <- ggplot(distanceUncorr[sort(distanceUncorr[,2],decreasing =
   labs(y = "Spatial correlation (km)") + geom_hline(aes(yintercept=round(mean(distanceUncorr[,2]),digits=2)   ),color="Black", linetype="dashed", size=0.3) +
   annotate("text", y = round(mean(distanceUncorr[,2]),digits=2) + 2 , x = 1 , label = paste0( round(mean(distanceUncorr[,2]),digits=2)," Km") , hjust = 0)
 
-pdf(file = paste0(resultsFolder.i,"/SpatialAutocorrelation.pdf"), width=12, height=8 )
-print(distanceUncorrPlot)
-dev.off()
+distanceUncorrPlot
 
 meanCorrDistance <- mean(distanceUncorr[,2])
-maxCorrDistance <- max(distanceUncorr[,2])
-occurrenceRecords <- spatialThinning(occurrenceRecords,rasterLayers, ifelse(meanCorrDistance > 25 , 25 , meanCorrDistance) )
 
-resultsSummary[1,"meanCorrDistance"] <- meanCorrDistance
-resultsSummary[1,"maxCorrDistance"] <- maxCorrDistance
-resultsSummary[1,"recordsFinal"] <- nrow(occurrenceRecords)
+# Prune occurrence recors 
+occurrenceRecords <- spatialThinning(occurrenceRecords,rasterLayers, meanCorrDistance )
+
 
 
